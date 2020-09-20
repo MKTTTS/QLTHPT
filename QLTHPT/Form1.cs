ea1e8ba5a2b1c39d2240b8a009212efd820baafd
MjnkNg0 <56630836+MjnkNg0@users.noreply.github.com>
2020-09-20 23:02:34 +0700

    FirstCommit

---
+++ QLTHPT/Form1.cs
@@ -0,0 +1,41 @@
+﻿using quanlyhocsinh;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTHPT
+{
+    public partial class Form1 : Form
+    {
+        public static int chooseGV_Stu = 0;
+        // gv =1; hs=2
+        SqlConnection conn = constringsql.getConnection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pb_gv_Click(object sender, EventArgs e)
+        {
+            chooseGV_Stu = 1;
+            this.Hide();
+            Formgiaovien fgv = new Formgiaovien();
+            fgv.ShowDialog();
+        }
+
+        private void pb_hs_Click(object sender, EventArgs e)
+        {
+            chooseGV_Stu = 2;
+            this.Hide();
+            Formhocsinh fhs = new Formhocsinh();
+            fhs.ShowDialog();
+        }
+    }
+}

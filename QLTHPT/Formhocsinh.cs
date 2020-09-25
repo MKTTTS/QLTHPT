c66ae45909eba4a20f5a65048469290dcaccfe1b
TLGKIEN <kienmathip1999@gmail.com>
2020-09-25 15:07:50 +0700

    design form Hoc Sinh

--- QLTHPT/Formhocsinh.cs
+++ QLTHPT/Formhocsinh.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QLTHPT
 {
     public partial class Formhocsinh : Form
     {
+
         public Formhocsinh()
         {
             InitializeComponent();
         }
+
     }
 }

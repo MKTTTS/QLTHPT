d5a79e68dd92a9870fe3b80823351e4ba6376bd6
TLGKIEN <kienmathip1999@gmail.com>
2020-09-25 23:14:21 +0700

    update déign

--- QLTHPT/Formhocsinh.Designer.cs
+++ QLTHPT/Formhocsinh.Designer.cs
@@ -34,99 +34,138 @@
             this.bt_suahs = new System.Windows.Forms.Button();
             this.bt_themhs = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHocSinh)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridViewHocSinh
             // 
             this.dataGridViewHocSinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewHocSinh.Location = new System.Drawing.Point(195, 14);
-            this.dataGridViewHocSinh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewHocSinh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewHocSinh.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewHocSinh.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridViewHocSinh.Name = "dataGridViewHocSinh";
             this.dataGridViewHocSinh.RowHeadersWidth = 51;
             this.dataGridViewHocSinh.RowTemplate.Height = 24;
-            this.dataGridViewHocSinh.Size = new System.Drawing.Size(638, 310);
+            this.dataGridViewHocSinh.Size = new System.Drawing.Size(835, 420);
             this.dataGridViewHocSinh.TabIndex = 6;
             // 
             // bt_timkiemhs
             // 
             this.bt_timkiemhs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bt_timkiemhs.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_timkiemhs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.bt_timkiemhs.Location = new System.Drawing.Point(45, 214);
-            this.bt_timkiemhs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_timkiemhs.ForeColor = System.Drawing.Color.Black;
+            this.bt_timkiemhs.Location = new System.Drawing.Point(502, 9);
+            this.bt_timkiemhs.Margin = new System.Windows.Forms.Padding(2);
             this.bt_timkiemhs.Name = "bt_timkiemhs";
             this.bt_timkiemhs.Size = new System.Drawing.Size(82, 30);
             this.bt_timkiemhs.TabIndex = 2;
-            this.bt_timkiemhs.Text = "Tìm Kiếm";
+            this.bt_timkiemhs.Text = "TÌM KIẾM";
             this.bt_timkiemhs.UseVisualStyleBackColor = false;
             // 
             // bt_xoahs
             // 
             this.bt_xoahs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bt_xoahs.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_xoahs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.bt_xoahs.Location = new System.Drawing.Point(45, 147);
-            this.bt_xoahs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_xoahs.ForeColor = System.Drawing.Color.Black;
+            this.bt_xoahs.Location = new System.Drawing.Point(361, 10);
+            this.bt_xoahs.Margin = new System.Windows.Forms.Padding(2);
             this.bt_xoahs.Name = "bt_xoahs";
             this.bt_xoahs.Size = new System.Drawing.Size(82, 30);
             this.bt_xoahs.TabIndex = 3;
-            this.bt_xoahs.Text = "Xóa";
+            this.bt_xoahs.Text = "XOÁ";
             this.bt_xoahs.UseVisualStyleBackColor = false;
             // 
             // bt_suahs
             // 
             this.bt_suahs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bt_suahs.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_suahs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.bt_suahs.Location = new System.Drawing.Point(45, 86);
-            this.bt_suahs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_suahs.ForeColor = System.Drawing.Color.Black;
+            this.bt_suahs.Location = new System.Drawing.Point(228, 9);
+            this.bt_suahs.Margin = new System.Windows.Forms.Padding(2);
             this.bt_suahs.Name = "bt_suahs";
             this.bt_suahs.Size = new System.Drawing.Size(82, 30);
             this.bt_suahs.TabIndex = 4;
-            this.bt_suahs.Text = "Sửa";
+            this.bt_suahs.Text = "SỬA";
             this.bt_suahs.UseVisualStyleBackColor = false;
             // 
             // bt_themhs
             // 
             this.bt_themhs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.bt_themhs.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bt_themhs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.bt_themhs.Location = new System.Drawing.Point(45, 32);
-            this.bt_themhs.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_themhs.ForeColor = System.Drawing.Color.Black;
+            this.bt_themhs.Location = new System.Drawing.Point(91, 9);
+            this.bt_themhs.Margin = new System.Windows.Forms.Padding(2);
             this.bt_themhs.Name = "bt_themhs";
             this.bt_themhs.Size = new System.Drawing.Size(82, 30);
             this.bt_themhs.TabIndex = 5;
-            this.bt_themhs.Text = "Thêm";
+            this.bt_themhs.Text = "THÊM";
             this.bt_themhs.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.button1.Location = new System.Drawing.Point(45, 278);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(639, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 29);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Back";
+            this.button1.Text = "QUAY LẠI";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.bt_timkiemhs);
+            this.panel1.Controls.Add(this.bt_themhs);
+            this.panel1.Controls.Add(this.bt_suahs);
+            this.panel1.Controls.Add(this.bt_xoahs);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 370);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(835, 50);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(835, 60);
+            this.panel2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(234, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DANH SÁCH HỌC SINH";
             // 
             // Formhocsinh
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(835, 420);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridViewHocSinh);
-            this.Controls.Add(this.bt_timkiemhs);
-            this.Controls.Add(this.bt_xoahs);
-            this.Controls.Add(this.bt_suahs);
-            this.Controls.Add(this.bt_themhs);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Formhocsinh";
             this.Text = "Formhocsinh";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHocSinh)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -139,5 +178,8 @@
         private System.Windows.Forms.Button bt_suahs;
         private System.Windows.Forms.Button bt_themhs;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }

77e423ee8c4e4347898473fea0d95f2fdf7c7c0c
Nguyễn Mạnh Sơn <56079016+sonvbhp199@users.noreply.github.com>
2020-12-21 20:54:58 +0700

    son

--- QLTHPT/FormSuaGV.cs
+++ QLTHPT/FormSuaGV.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using quanlyhocsinh;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,43 @@ namespace QLTHPT
 {
     public partial class FormSuaGV : Form
     {
-        public FormSuaGV()
+        public FormSuaGV(string manv, int index, DataGridView datagv)
         {
             InitializeComponent();
+            txt_magv.Text = datagv.Rows[index].Cells[0].Value.ToString();
+            txt_hoten.Text = datagv.Rows[index].Cells[1].Value.ToString();
+            txt_sdt.Text = datagv.Rows[index].Cells[2].Value.ToString();
+            txt_chuyenmon.Text = datagv.Rows[index].Cells[3].Value.ToString();
+            txt_gioitinh.Text = datagv.Rows[index].Cells[4].Value.ToString();
+            txt_noisinh.Text = datagv.Rows[index].Cells[5].Value.ToString();
+            txt_magv.Enabled = false;
+        }
+
+        private void bt_luu_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = constringsql.getConnection();
+            conn.Open();
+            string strQueryDanhSach = "UPDATE dbo.GIAOVIEN SET HOTEN=@HOTEN,SODIENTHOAI=@SODIENTHOAI,CHUYENMON=@CHUYENMON," +
+                "GIOITINH=@GIOITINH,NOISINH=@NOISINH WHERE MAGIAOVIEN = @MAGV ";
+            SqlCommand comm = new SqlCommand(strQueryDanhSach);
+            comm.Connection = conn;
+            comm.Parameters.AddWithValue("@MAGV", txt_magv.Text);
+            comm.Parameters.AddWithValue("@HOTEN", txt_hoten.Text);
+            comm.Parameters.AddWithValue("@GIOITINH", txt_gioitinh.Text);
+            comm.Parameters.AddWithValue("@NOISINH", txt_noisinh.Text);
+            comm.Parameters.AddWithValue("@CHUYENMON", txt_chuyenmon.Text);
+            comm.Parameters.AddWithValue("@SODIENTHOAI", txt_sdt.Text);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Sửa thành công!", "Thông báo!");
+            this.Close();
+        }
+
+        private void bt_back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Formgiaovien fgv = new Formgiaovien();
+            fgv.ShowDialog();
         }
     }
 }
--- QLTHPT/FormSuaHS.cs
+++ QLTHPT/FormSuaHS.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using quanlyhocsinh;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,42 @@ namespace QLTHPT
 {
     public partial class FormSuaHS : Form
     {
-        public FormSuaHS()
+        public FormSuaHS(string mahs, int index, DataGridView datahs)
         {
             InitializeComponent();
+            txt_mahs.Text = datahs.Rows[index].Cells[0].Value.ToString();
+            txt_hoten.Text = datahs.Rows[index].Cells[1].Value.ToString();
+            txt_gioitinh.Text = datahs.Rows[index].Cells[2].Value.ToString();
+            dateTimePicker1.Value = Convert.ToDateTime(datahs.Rows[index].Cells[3].Value.ToString());
+            txt_quequan.Text = datahs.Rows[index].Cells[4].Value.ToString();
+            txt_mahs.Enabled = false;
+        }
+
+        private void bt_luu_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = constringsql.getConnection();
+            conn.Open();
+            string strQueryDanhSach = "UPDATE dbo.HOCSINH SET HOTEN=@HOTEN,GIOITINH=@GIOITINH,NGAYSINH=@NGAYSINH,NOISINH=@NOISINH" +
+                " WHERE MAHOCSINH = @MAHOCSINH  ";
+            SqlCommand comm = new SqlCommand(strQueryDanhSach);
+            comm.Connection = conn;
+            comm.Parameters.AddWithValue("@MAHOCSINH", txt_mahs.Text);
+            comm.Parameters.AddWithValue("@HOTEN", txt_hoten.Text);
+            comm.Parameters.AddWithValue("@GIOITINH", txt_gioitinh.Text);
+            //comm.Parameters.AddWithValue("@NGAYSINH", dateTimePicker1.Value.ToString("dd/MM/yyyy"));
+            comm.Parameters.AddWithValue("@NGAYSINH", dateTimePicker1.Value.ToShortDateString());
+            comm.Parameters.AddWithValue("@NOISINH", txt_quequan.Text);
+            comm.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Sửa thành công!", "Thông báo!");
+            this.Close();
+        }
+
+        private void bt_back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Formhocsinh fhs = new Formhocsinh();
+            fhs.ShowDialog();
         }
     }
 }
